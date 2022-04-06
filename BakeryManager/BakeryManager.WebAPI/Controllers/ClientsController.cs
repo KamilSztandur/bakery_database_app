a153b5cd62d95f663eb49a2f6596908c24e437a5
KamilSztandur <kamil.sztandur@vp.pl>
2022-04-06 21:48:51 +0200

    Add ClientsController and modify Startup.cs

---
+++ BakeryManager/BakeryManager.WebAPI/Controllers/ClientsController.cs
@@ -0,0 +1,62 @@
+﻿using BakeryManager.Infrastructure.Commands;
+using BakeryManager.Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BakeryManager.WebAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class ClientsController : Controller
+{
+    private readonly IClientService _clientService;
+    
+    public ClientsController(IClientService clientService)
+    {
+        _clientService = clientService;
+    }
+
+    [HttpGet]
+    public async Task<string> BrowseAll()
+    {
+        var result = await _clientService.BrowseAll();
+
+        var resultAsJson = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        return resultAsJson;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<string> GetClient(int id)
+    {
+       var result = await _clientService.GetClient(id);
+
+        var resultAsJson = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        return resultAsJson;
+    }
+
+    [HttpPost]
+    public async Task<string> AddClient([FromBody] CreateClient _client)
+    {
+        var result = await _clientService.AddClient(_client);
+        
+        var resultAsJson = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        return resultAsJson;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<string> UpdateClient([FromBody] CreateClient _client, int id)
+    {
+        var result = await _clientService.UpdateClient(id, _client);
+
+        var resultAsJson = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        return resultAsJson;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<string> DeleteClient(int id)
+    {
+        var result = await _clientService.DeleteClient(id);
+        var resultAsJson = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        
+        return resultAsJson;
+    }
+}
--- BakeryManager/BakeryManager.WebAPI/Program.cs
+++ BakeryManager/BakeryManager.WebAPI/Program.cs
@@ -1,25 +1,51 @@
+using BakeryManager.Core.Repositories;
+using BakeryManager.Infrastructure.Repositories;
+using BakeryManager.Infrastructure.Services;
+using BakeryManager.Infrastructure.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IClientService, ClientService>();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<IBakeryRepository, BakeryRepository>();
+builder.Services.AddScoped<IBakeryService, BakeryService>();
+
+builder.Services.AddScoped<IReceiptRepository, ReceiptRepository>();
+builder.Services.AddScoped<IReceiptService, ReceiptService>();
+
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(
+        "Data Source=.\\sqlexpress;Initial Catalog=bakery;Integrated Security=SSPI;",
+            b => b.MigrationsAssembly("BakeryManager.WebAPI")
+        )
+);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseRouting();
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
