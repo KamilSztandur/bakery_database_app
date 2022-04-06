e019675150707bbf63d961b415bec161b37f3254
KamilSztandur <kamil.sztandur@vp.pl>
2022-04-06 14:07:42 +0200

    Redeploy ASP.NET Core web app

--- BakeryManager/BakeryManager/BakeryManager.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BakeryManager.WebApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
